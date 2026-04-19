2971348e91f508405c8fe5a7336ddea866a51b12
eyalm <eyalme1@walla.co.il>
2026-04-19 23:16:09 +0300

    update filter

--- src/main/java/com/example/model/Filter.java
+++ src/main/java/com/example/model/Filter.java
@@ -7,18 +7,40 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Filter {
+    @JsonProperty("_id")
     private String id;
     private String name;
-    private String rule;
+    private LocalDateTime createDate;
+    private String description;
+    private String owner;
+    private String s3Path;
+    private boolean approved;
+    @JsonProperty("isActive")
+    private boolean active;
     @JsonProperty("isRevertible")
     private boolean revertible;
+    private String version;
+    private List<AdditionalParam> additionalParams;
 
-    @JsonProperty("isInclude")
-    private boolean include;
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    public static class AdditionalParam {
+        private String name;
+        private String description;
+        private String type;
+        @JsonProperty("isArray")
+        private boolean array;
+    }
 }
